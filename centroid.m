0d81df4892a0a0c660aa0517fdf96efe6c881f39
egyptianf <walied2@protonmail.com>
2020-02-28 16:22:30 +0200

    first commit

---
+++ centroid.m
@@ -0,0 +1,2 @@
+%Code for extracting the feature vector from the images
+run('load_images.m')
---
+++ read_dataset.m
@@ -0,0 +1,20 @@
+# Prevent Octave from thinking that this
+# is a function file:
+
+1;
+function images = read_data()
+  images = cell(100, 1);
+
+  dir = "dataset/";
+  for i=1:10
+    for j=1:10
+      img_loc = strcat(dir, num2str(i-1), "_", num2str(j), ".bmp");
+      image = imread(img_loc);
+      image = ! image;
+      image_cell = mat2cell(image, size(image)(1), size(image)(2));
+      images(((i-1)*10) + j) = image_cell;
+    end
+  end
+endfunction
+images = read_data();
+save images.mat images
