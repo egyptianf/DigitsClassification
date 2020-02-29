ad0799737956d1e716e89bef064ea8cccc019b54
egyptianf <walied2@protonmail.com>
2020-02-29 05:20:19 +0200

    almost finished the dividing function

--- centroid.m
+++ centroid.m
@@ -3,14 +3,27 @@ run('load_images.m');
 function blocks = divide(image, rows, cols)
   blocks = cell(rows*cols, 1);
   [imrows imcols] = size(image);
-  block_rows = imrows / rows;
-  block_cols = imcols / cols;
+  block_rows = floor(imrows / rows);
+  block_cols = floor(imcols / cols);
   for i=1:rows
     for j=1:cols
-      %first block
-      block = image(((i-1)*9)+1:block_rows*i, ((j-1)*9)+1:block_cols*j);
-      blocks(((i-1)*cols) + j) = block;
+      if(i < rows & j < cols)
+        block = image(((i-1)*block_rows)+1:block_rows*i, ((j-1)*block_cols)+1:block_cols*j);
+      %To handle edge cases
+      else
+        if i==rows & j!=cols
+          block = image(((i-1)*block_rows)+1:imrows, ((j-1)*block_cols)+1:block_cols*j);
+        endif
+        if j==cols & i!=rows
+          block = image(((i-1)*block_rows)+1:block_rows*i, ((j-1)*block_cols)+1:imcols);
+        endif
+        if i==rows & j==cols
+          block = image(((i-1)*block_rows)+1:imrows, ((j-1)*block_cols)+1:imcols);
+        endif
+      endif
+      blocks(((i-1)*cols) + j) = block; 
     endfor
   endfor
 endfunction
-blocks = divide(images{1}, 3, 3);
+size(images{2})
+blocks = divide(images{3}, 3, 3);
