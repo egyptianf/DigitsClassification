55de35e7c06d0c4b2503f0dcb9f03b4233a42643
egyptianf <walied2@protonmail.com>
2020-03-01 04:19:40 +0200

    evaluation function

--- centroid.m
+++ centroid.m
@@ -7,17 +7,17 @@ function blocks = divide(image, rows, cols)
   block_cols = floor(imcols / cols);
   for i=1:rows
     for j=1:cols
-      if(i < rows & j < cols)
+      if i < rows && j < cols
         block = image(((i-1)*block_rows)+1:block_rows*i, ((j-1)*block_cols)+1:block_cols*j);
       %To handle edge cases
       else
-        if i==rows & j!=cols
+        if i==rows && j!=cols
           block = image(((i-1)*block_rows)+1:imrows, ((j-1)*block_cols)+1:block_cols*j);
         endif
-        if j==cols & i!=rows
+        if j==cols && i!=rows
           block = image(((i-1)*block_rows)+1:block_rows*i, ((j-1)*block_cols)+1:imcols);
         endif
-        if i==rows & j==cols
+        if i==rows && j==cols
           block = image(((i-1)*block_rows)+1:imrows, ((j-1)*block_cols)+1:imcols);
         endif
       endif
@@ -39,8 +39,13 @@ function [x_bar y_bar] = get_block_centroid(block)
   x_bar = x_bar / blk_cols;
   y_bar = y_bar / blk_rows;
 endfunction
-
-
+function feature_matrix = get_feature_matrix(blocks, rows, cols)
+  feature_matrix = zeros(rows*cols, 2);
+  for i=1:rows*cols
+    [x_bar y_bar] = get_block_centroid(blocks{i});
+    feature_matrix(i, :) = [x_bar y_bar]; 
+  endfor
+endfunction
 rows=3;
 cols=3;
 [num_of_images ~] = size(images);
@@ -48,10 +53,26 @@ feature_matrices = cell(100, 1);
 for k=1:num_of_images
   blocks = divide(images{k}, rows, cols);
   %Trying on each block of the first image
-  feature_matrix = zeros(rows*cols, 2);
-  for i=1:rows*cols
-    [x_bar y_bar] = get_block_centroid(blocks{i});
-    feature_matrix(i, :) = [x_bar y_bar]; 
-  endfor
+  feature_matrix = get_feature_matrix(blocks, rows, cols);
   feature_matrices{k} = feature_matrix;
 endfor
+
+
+%Now the evaluation function
+function class=evaluate(new_image, feature_matrices, newim_rows, newim_cols)
+  imblocks = divide(new_image, newim_rows, newim_cols);
+  imfeature_matrix = get_feature_matrix(imblocks, newim_rows, newim_cols);
+  min_difference = sum((imfeature_matrix - feature_matrices{1}) .^ 2, 2);
+  [size ~] = size(feature_matrices);
+  index = 1;
+  for i=2:size
+    diff = sum((imfeature_matrix - feature_matrices{i}) .^ 2, 2);
+    if diff < min_difference
+      min_difference = diff;
+      index = i;
+    endif
+  endfor
+  class = index;
+endfunction
+
+c = evaluate(images{10}, feature_matrices, rows, cols);
